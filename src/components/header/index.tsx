29e8b4f03e96491896c7009ae6e4606fbde05669
WebCrazy003 <RayCoaster99001@gmail.com>
2023-05-14 13:52:47 -0400

    Remove Logo and footer component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import styles from "./index.module.css";
 
-import { Logo } from "@components";
-
 export const Header: React.FC = ({ title, showNav }) => {
   return (
     <div className={styles.header}>
