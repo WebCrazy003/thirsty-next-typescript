7ac3ad5b3196d9c449256d5bad2d9100709ecb24
WebCrazy003 <RayCoaster99001@gmail.com>
2023-05-16 05:45:26 -0400

    Add useDebounce custom hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { Layout, SearchInput, ResultItem } from "@components";
 import styles from "@styles/drink.module.css";
+import { useDebounce } from "@utils/hooks";
 
 const Home: React.FC = () => {
   const [searchResult, setSearchResult] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const debouncedValue = useDebounce(searchText, 500);
 
-  const onInputChange = async (searchText: string) => {
-    const response = await fetch(
-      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchText}`,
-    );
-    const { drinks } = await response.json();
-    setSearchResult(drinks || []);
-  };
+  useEffect(() => {
+    const getResult = async () => {
+      const response = await fetch(
+        `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${debouncedValue}`,
+      );
+      const { drinks } = await response.json();
+      setSearchResult(drinks || []);
+    };
+
+    getResult();
+  }, [debouncedValue]);
 
   return (
     <Layout pageTitle="Thirsty">
       <div className={styles.listWrapper}>
         <SearchInput
-          onChange={onInputChange}
+          onChange={(text) => setSearchText(text)}
           placeholder="Find a drink"
           defaultValue=""
         />
